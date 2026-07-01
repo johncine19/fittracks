2a91388e4214a43bc8986ce116343018deb391c9
Johncine Martil <johncine.martil@nmsc.edu.ph>
2026-07-01 16:41:35 +0800

    remove migrate file

--- migrations/0002_nullable_training_plan_trainer.sql
+++
@@ -1,5 +0,0 @@
--- Allow system-generated workout plans without an assigned trainer.
-ALTER TABLE `training_plans` DROP FOREIGN KEY `fk_trainingplan_coach`;
-ALTER TABLE `training_plans` MODIFY `trainer_id` int UNSIGNED NULL DEFAULT NULL;
-ALTER TABLE `training_plans`
-  ADD CONSTRAINT `fk_trainingplan_coach` FOREIGN KEY (`trainer_id`) REFERENCES `trainer_profiles` (`trainer_id`) ON DELETE CASCADE;
--- migrations/0006_cache_engagement_scores.sql
+++
@@ -1,5 +0,0 @@
--- Add cached engagement score columns to users table
-
-ALTER TABLE users
-  ADD COLUMN engagement_score TINYINT UNSIGNED NULL DEFAULT NULL,
-  ADD COLUMN engagement_computed_at DATETIME NULL DEFAULT NULL;
