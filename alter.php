4bc99e776af027e62c95391701c738ee9f0253ca
Johncine Martil <johncine.martil@nmsc.edu.ph>
2026-07-05 05:26:52 +0800

    .

--- alter.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $pdo = new PDO("mysql:host=127.0.0.1;dbname=fittrack_db", "root", "johncine");
-    $pdo->exec("ALTER TABLE walk_in_transactions ADD COLUMN payment_method ENUM('cash', 'gcash') NOT NULL DEFAULT 'cash' AFTER amount_paid");
-    echo "Success!";
-} catch (Exception $e) {
-    echo "Error: " . $e->getMessage();
-}
