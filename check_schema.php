8ca33ae89abc18c39afaaf5e805a2db510edbb3c
Johncine Martil <johncine.martil@nmsc.edu.ph>
2026-07-09 10:13:56 +0800

    delete temporary scripts

--- check_schema.php
+++
@@ -1,4 +0,0 @@
-<?php
-require __DIR__ . '/core/bootstrap.php';
-$schema = db()->query("SHOW CREATE TABLE trainer_profiles")->fetch();
-print_r($schema);
--- dump_cols.php
+++
@@ -1,3 +0,0 @@
-<?php
-require 'core/bootstrap.php';
-print_r(db()->query("SHOW COLUMNS FROM trainer_assignments")->fetchAll(PDO::FETCH_ASSOC));
