61b175bb2397b4aa8ed5c92ea880810bea70449d
Johncine Martil <johncine.martil@nmsc.edu.ph>
2026-07-14 16:32:55 +0800

    delete

--- schema_check.php
+++
@@ -1,4 +0,0 @@
-<?php
-require 'core/bootstrap.php';
-$stmt = db()->query('SHOW CREATE TABLE member_profiles');
-print_r($stmt->fetch());
--- show_tables.php
+++
@@ -1,4 +0,0 @@
-<?php
-require __DIR__ . '/core/bootstrap.php';
-$pdo = db();
-print_r($pdo->query('SHOW TABLES')->fetchAll(PDO::FETCH_COLUMN));
