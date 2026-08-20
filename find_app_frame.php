5aea21aad2adbc7db7c4251f5658b8581d8402b0
Johncine Martil <johncine.martil@nmsc.edu.ph>
2026-08-20 09:16:16 +0800

    .

--- find_app_frame.php
+++
@@ -1,8 +0,0 @@
-<?php
-$lines = file('assets/app.css');
-foreach ($lines as $i => $line) {
-    if (strpos($line, '.app-frame') !== false) {
-        echo ($i + 1) . ": " . trim($line) . "\n";
-    }
-}
-?>
